7dceb917db6d4cd6c77d0d267ee1f0dde10fd064
C0B23154 <c0b23154c4@edu.teu.ac.jp>
2024-06-11 16:03:54 +0900

    4

--- pygame_image.py
+++ pygame_image.py
@@ -18,6 +18,10 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])    #背景画像を表示せよ．
+        #横300，縦200の位置に，こうかとんSurfaceをblitせよ．
+        k3_rct = k3_img.get_rect()  #画像Surfaceに対応する画像Rectを取得する
+        k3_rct.center = 300, 200    
+        screen.blit(k3_img, k3_rct)     #画像SurfaceをスクリーンSurfaceにRectに従って貼り付ける
         pg.display.update()
         tmr += 1        
         clock.tick(10)
