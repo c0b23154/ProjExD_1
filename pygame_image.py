68746529308d4b90edafee66ea4f9eff89090644
C0B23154 <c0b23154c4@edu.teu.ac.jp>
2024-06-11 16:32:11 +0900

    7

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")     #背景画像「pg_bg.jpg」（画像サイズ：幅1600 高さ900）を読み込み，Surfaceを生成せよ．
+    bg_flip = pg.transform.flip(bg_img, True, False)
     k3_img = pg.image.load("fig/3.png")     #こうかとん画像「3.png」を読み込み，Surfaceを生成せよ．
     k3_img = pg.transform.flip(k3_img, True, False)   #そして，左右を反転せよ．
     tmr = 0
@@ -17,8 +18,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%800
+        x = tmr%4800         #こうかとんが画面右に向かって進んでいるように見せるために，背景画像を右から左に動くように，背景画像の横座標を修正せよ．そして，1600フレーム後に背景画像が間延びしないように，工夫せよ．
         screen.blit(bg_img, [-x, 0])    #背景画像を表示せよ．
+        screen.blit(bg_flip, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_flip, [-x+4800, 0])
         #横300，縦200の位置に，こうかとんSurfaceをblitせよ．
         k3_rct = k3_img.get_rect()  #画像Surfaceに対応する画像Rectを取得する
         k3_rct.center = 300, 200    
